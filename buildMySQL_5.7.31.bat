4d00f853276cf6692363e488ec50e83f8e13fc7a
Kevin Mommens <kevin.mommens@infrabel.be>
2022-08-12 10:35:43 +0200

    Removal of MySQL container after MySQL build

--- buildMySQL_5.7.31.bat
+++ buildMySQL_5.7.31.bat
@@ -1,7 +1,13 @@
 @ECHO OFF
-echo Building image: c++ compiler for MySQL and make MySQL 5.7.31
+echo [INFO] Building image: c++ compiler for MySQL and make MySQL 5.7.31
 docker build ./MySQL_5_7_31 -t patools/mysql:5.7.31
-echo Starting container: image with MySQL package build
-docker run --rm patools/mysql:5.7.31 --name mysql_5_7_31
+echo [INFO] Running container with MySQL build image
+docker run --name mysql_5_7_31 patools/mysql:5.7.31
+mkdir dependencies
+echo [INFO] Getting MySQL build from container
 docker cp mysql_5_7_31:/usr/local/mysql-5.7.31/mysql-5.7.31-linux-x86_64.tar.gz ./dependencies
+echo [INFO] Stopping container with MySQL build image
+docker stop mysql_5_7_31
+echo [INFO] Removing container with MySQL build image
+docker rm mysql_5_7_31
 cmd /k
